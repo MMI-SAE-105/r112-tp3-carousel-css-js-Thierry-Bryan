689f161bac22df9d8dad22d871fa88ed3e955c67
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2024-11-19 16:44:38 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 // *** CAROUSEL ***
 // TODO
+const carousel = document.querySelector("div>div");
+const prevButton = document.querySelector("div>button");
+const nextButton = document.querySelector("div>button+button");
+
+// Largeur de défilement d’un item
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({left: -260, behavior: "smooth" });
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({left: +260, behavior: "smooth" });
+  });
+}
+
+
+//carousel.scrollBy({ left: +260, behavior: "smooth" });
